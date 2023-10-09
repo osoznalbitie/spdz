d46caf9735d2a8a9d026e83c25283eb4537b8738
osoznalbitie <alexcolinson1@gmail.com>
2023-10-09 22:43:16 +0400

    fix bags

---
+++ controller/add_user.php
@@ -0,0 +1,19 @@
+<?php
+require_once('../model/User.php');
+require_once('../model/Adress.php');
+session_start();
+foreach($_SESSION['userArray'] as $user)
+    if ($user->getName() == $_POST['name']) {
+        echo '<a href="/panel.php">Назад</a> <br>';
+        die('Пользователь уже существует');
+    }
+
+$newAdress = new Adress(count($_SESSION['adressArray']), $_POST['city'],$_POST['country']);
+array_push($_SESSION['adressArray'],$newAdress);
+
+++$_SESSION['currentID'];
+$newUser = new User($_SESSION['currentID'],$_POST['name'],$_POST['age'], $_POST['password'],count($_SESSION['adressArray'])-1,$_POST['role']);
+array_push($_SESSION['userArray'], $newUser);
+
+header('Location:/panel.php');
+?>
